08c4104b8738520f5cddeb94a3fc15fb38a2160e
Kingsley Ekoh-Ordan <kingsleybox@yahoo.com>
2023-06-15 11:58:15 +0100

    fix null game name bug

--- be/WhotGame.Core/Data/Models/Game.cs
+++ be/WhotGame.Core/Data/Models/Game.cs
@@ -5,7 +5,7 @@ namespace WhotGame.Core.Data.Models
     public class Game
     {
         public long Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public DateTime DateCreated { get; set; }
         public long CreatorId { get; set; }
         public GameStatus Status { get; set; }
