8e820d1eac1bea62a0cb6f71835611c4750b583f
kigold <kingsleybox@yahoo.com>
2024-08-04 00:00:17 +0100

    add nav bar, set route

--- fe/whotapp2/src/app/app.component.ts
+++ fe/whotapp2/src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { User } from './models/auth';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'whotapp2';
+  profile: User = { 
+    name: "Kaycee",
+    avatar: "https://google/com/image",
+    id: 1,
+    username: "kaycee"
+  }
 }
--- fe/whotapp2/src/app/app.routes.ts
+++ fe/whotapp2/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { authguardGuard } from './utils/authguard.guard';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'game/:id', component: GameComponent, canActivate: [authguardGuard] },    
+    { path: 'game/:id', component: GameComponent},  
+    // { path: 'game/:id', component: GameComponent, canActivate: [authguardGuard] },   
     { path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
